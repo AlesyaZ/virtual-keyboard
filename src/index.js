1a580bd6c5a0e3e1453a23560ab4c5e7ddac8dbb
alesyaz <alesya.zarembo@mail.ru>
2022-05-09 14:49:07 +0300

    feat: add change language and capsLock

--- src/index.js
+++ src/index.js
@@ -69,6 +69,140 @@ document.querySelector('.keyboard').innerHTML = out;
 
 const keykey = document.querySelectorAll('.keyK');
 
+let isHighRegister = false;
+let isRuLang = false;
+let flag = true;
+let checkCtrl = false;
+
+keykey.forEach((el, i) => {
+  el.classList.add(codes[i]);
+});
+
+document.addEventListener('keyup', (event) => {
+  if (codes) {
+    document.querySelector(`.${event.code}.act`).classList.remove('active');
+  }
+});
+
+const caseDownRu = document.querySelectorAll('.keyK.ru.keyDown');
+const caseUPEn = document.querySelectorAll('.keyK.en.keyUp');
+const caseDownEn = document.querySelectorAll('.keyK.en.keyDown');
+const caseUPRu = document.querySelectorAll('.keyK.ru.keyUp');
+
+function isCapslock() {
+  if (isRuLang) {
+    if (isHighRegister === false) {
+      caseDownRu.forEach((e) => e.classList.add('hidden'));
+      caseDownRu.forEach((e) => e.classList.remove('act'));
+      caseUPRu.forEach((e) => e.classList.remove('hidden'));
+      caseUPRu.forEach((e) => e.classList.add('act'));
+      isHighRegister = true;
+    } else {
+      caseDownRu.forEach((e) => e.classList.remove('hidden'));
+      caseDownRu.forEach((e) => e.classList.add('act'));
+      caseUPRu.forEach((e) => e.classList.add('hidden'));
+      caseUPRu.forEach((e) => e.classList.remove('act'));
+      isHighRegister = false;
+    }
+  } else if (isHighRegister === false) {
+    caseDownEn.forEach((e) => e.classList.add('hidden'));
+    caseDownEn.forEach((e) => e.classList.remove('act'));
+    caseUPEn.forEach((e) => e.classList.remove('hidden'));
+    caseUPEn.forEach((e) => e.classList.add('act'));
+    isHighRegister = true;
+  } else {
+    caseDownEn.forEach((e) => e.classList.remove('hidden'));
+    caseDownEn.forEach((e) => e.classList.add('act'));
+    caseUPEn.forEach((e) => e.classList.add('hidden'));
+    caseUPEn.forEach((e) => e.classList.remove('act'));
+    isHighRegister = false;
+  }
+}
+
+function isShift() {
+  isCapslock();
+}
+
+const ruBtns = document.querySelectorAll('.keyK.ru');
+const enBtns = document.querySelectorAll('.keyK.en');
+
+function change() {
+  const language = JSON.parse(isRuLang);
+  localStorage.setItem('isRuLang', language);
+  localStorage.getItem('isRuLang');
+}
+
+const changeLang = () => {
+  if (isRuLang) {
+    ruBtns.forEach((e) => {
+      e.classList.add('hidden');
+    });
+    enBtns.forEach((e) => {
+      e.classList.remove('hidden');
+    });
+
+    isRuLang = false;
+    if (isHighRegister) {
+      caseDownEn.forEach((e) => e.classList.add('hidden'));
+      caseDownEn.forEach((e) => e.classList.remove('act'));
+      caseUPEn.forEach((e) => e.classList.remove('hidden'));
+      caseUPEn.forEach((e) => e.classList.add('act'));
+
+      isHighRegister = true;
+    } else {
+      caseDownEn.forEach((e) => e.classList.remove('hidden'));
+      caseDownEn.forEach((e) => e.classList.add('act'));
+      caseUPEn.forEach((e) => e.classList.add('hidden'));
+      caseUPEn.forEach((e) => e.classList.remove('act'));
+      isHighRegister = false;
+    }
+  } else {
+    ruBtns.forEach((e) => {
+      e.classList.remove('hidden');
+    });
+    enBtns.forEach((e) => {
+      e.classList.add('hidden');
+    });
+
+    caseUPEn.forEach((e) => {
+      e.classList.add('hidden');
+      e.classList.remove('act');
+    });
+    caseDownEn.forEach((e) => {
+      e.classList.add('hidden');
+      e.classList.remove('act');
+    });
+
+    isRuLang = true;
+    if (isHighRegister) {
+      caseDownRu.forEach((e) => e.classList.add('hidden'));
+      caseDownRu.forEach((e) => e.classList.remove('act'));
+      caseUPRu.forEach((e) => e.classList.remove('hidden'));
+      caseUPRu.forEach((e) => e.classList.add('act'));
+      isHighRegister = true;
+    } else {
+      caseDownRu.forEach((e) => e.classList.remove('hidden'));
+      caseDownRu.forEach((e) => e.classList.add('act'));
+      caseUPRu.forEach((e) => e.classList.add('hidden'));
+      caseUPRu.forEach((e) => e.classList.remove('act'));
+      isHighRegister = false;
+    }
+  }
+};
+changeLang();
+
+function input(letter) {
+  const arr = textArea.value.split('');
+  const position = textArea.selectionStart;
+  arr.splice(position, 0, letter);
+  textArea.value = arr.join('');
+  textArea.selectionStart = position + 1;
+  textArea.selectionEnd = position + 1;
+}
+
+textArea.focus();
+textArea.addEventListener('blur', () => textArea.focus());
+
 const footer = document.createElement('footer');
 footer.innerHTML = '<p>Для переключения языка комбинация: левые Ctrl + Alt Left <br >OS Windows</p>';
 document.body.appendChild(footer);
